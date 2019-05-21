af000cd26fa35ea06a7fc7d0fee5fdad8127a634
long8v <ckleckle@gmail.com>
2019-05-21 15:21:47 +0900

    project started

--- NLP.py
+++ NLP.py
@@ -6,8 +6,14 @@ Created on Fri May 17 20:22:34 2019
 """
 
 from matplotlib import font_manager, rc
+import winsound
 
 def font_setting():
     font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
     rc('font', family=font_name)
     return None
+
+def make_sount():	
+	duration = 1000  # milliseconds
+	freq = 440  # Hz
+	winsound.Beep(freq, duration)
