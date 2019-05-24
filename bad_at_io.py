6b84a55550adb65e00b1627c1871c6710ed6981c
long8v <ckleckle@gmail.com>
2019-05-25 03:09:44 +0900

    슬슬 노트북이 울부짖기 시작했다

--- bad_at_io.py
+++ bad_at_io.py
@@ -16,6 +16,6 @@ def pickle_save(filename,object_name):
     with open(filename, 'wb') as f:
         pickle.dump(object_name, f)
         
-def pickle_open(filename,object_name):
+def pickle_open(filename):
     with open(filename, 'rb') as f:
         return pickle.load(f)
