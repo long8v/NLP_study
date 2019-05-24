ef727dd804a7ee7d7b39f98a8156cccf21d02c6b
long8v <ckleckle@gmail.com>
2019-05-25 02:16:42 +0900

    tfidf

---
+++ bad_at_io.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 25 01:50:02 2019
+
+@author: long8v
+"""
+
+import pickle
+
+def txt_open(filename):
+    with open (filename) as fp:
+        txt += fp.read()
+    return txt
+
+def pickle_save(filename,object_name):
+    with open(filename, 'wb') as f:
+        pickle.dump(object_name, f)
+        
+def pickle_open(filename,object_name):
+    with open(filename, 'rb') as f:
+        return pickle.load(f)
